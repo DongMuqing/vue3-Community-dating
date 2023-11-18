c1d05f23193d145a57e8b7320ff6ff02755731d7
DongMuqing <131415000@163.com>
2023-11-18 21:02:29 +0800

    :construction_worker:写入后台路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { ElMessage } from 'element-plus'
-
+import tokens from '../api/open/token'
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-//   "history": createWebHistory()
-// "hash": createWebHashHistory()
-// "abstract": createMemoryHistory()
-//新版写法
+  //   "history": createWebHistory()
+  // "hash": createWebHashHistory()
+  // "abstract": createMemoryHistory()
+  //新版写法
   history: createWebHistory(),
   routes: [
     { path: '', redirect: '/home' },
@@ -16,20 +16,35 @@ const router = createRouter({
       // 不会触发子路由 所以删掉父路由中的name
       // name: 'home',
       component: () => import('../views/ForegroundPage/Home.vue'),
-      children:[
-        { path: '', component: ()=> import('../views/ForegroundPage/Post.vue'), meta: { keepAlive: true } },
-        { path: 'post', component:()=> import('../views/ForegroundPage/Post.vue'),meta: { keepAlive: true } },
-        { path: 'article', component:()=> import('../views/ForegroundPage/Article.vue'),meta: { keepAlive: true } },
-        { path: 'articles/:id', name: 'articles', component:()=> import('../views/ForegroundPage/ArticleContent.vue') , props: true },
-        { path: 'friendlink', component:()=> import('../views/ForegroundPage/Friendlink.vue') , meta: { keepAlive: true } },
-        { path: 'leavemessage', component: ()=> import('../views/ForegroundPage/Leavemessage.vue'), meta: { keepAlive: true } },
-        { path: '/subfriend', component:()=> import('../views/ForegroundPage/SubmitFriendlink.vue') , meta: { keepAlive: true } },
+      children: [
+        { path: '', component: () => import('../views/ForegroundPage/Post.vue'), meta: { keepAlive: true } },
+        { path: 'post', component: () => import('../views/ForegroundPage/Post.vue'), meta: { keepAlive: true } },
+        { path: 'article', component: () => import('../views/ForegroundPage/Article.vue'), meta: { keepAlive: true } },
+        { path: 'articles/:id', name: 'articles', component: () => import('../views/ForegroundPage/ArticleContent.vue'), props: true },
+        { path: 'friendlink', component: () => import('../views/ForegroundPage/Friendlink.vue'), meta: { keepAlive: true } },
+        { path: 'leavemessage', component: () => import('../views/ForegroundPage/Leavemessage.vue'), meta: { keepAlive: true } },
+        { path: '/subfriend', component: () => import('../views/ForegroundPage/SubmitFriendlink.vue'), meta: { keepAlive: true } },
       ]
     },
-     //做404跳转
-     {path:'/404',component:()=>import('../views/ForegroundPage/404.vue')},
-     //没有匹配道则重定向到404页面
-     { path: "/:pathMatch(.*)", redirect: "/404", name: "notMatch", hidden: true },
+    { path: '/login', component: () => import('../views/BackgroundPage/Login.vue') },
+    {
+      path: '/main', component: () => import('../views/BackgroundPage/Main.vue'),
+      children: [
+        { path: '', component: () => import('../views/BackgroundPage/Home.vue') , meta: { keepAlive: true }},
+        { path: 'home', component: () => import('../views/BackgroundPage/Home.vue') , meta: { keepAlive: true }},
+        { path: 'post', component: () => import('../views/BackgroundPage/Post.vue') , meta: { keepAlive: true }},
+        { path: 'article', component: () => import('../views/BackgroundPage/Article.vue') , meta: { keepAlive: true }},
+        { path: 'publish', component: () => import('../views/BackgroundPage/Publish.vue') , meta: { keepAlive: true }},
+        { path: 'user', component: () => import('../views/BackgroundPage/User.vue') , meta: { keepAlive: true }},
+        { path: 'visitorInfo', component: () => import('../views/BackgroundPage/VisitorInfo.vue') , meta: { keepAlive: true }},
+        { path: 'comment', component: () => import('../views/BackgroundPage/Comment.vue') , meta: { keepAlive: true }},
+        { path: 'upload',component: () => import('../views/BackgroundPage/Upload.vue') , meta: { keepAlive: true }},
+      ]
+    },
+     //做404跳转 需要放到路由的最后面
+     { path: '/404', component: () => import('../views/ForegroundPage/404.vue') },
+    //没有匹配道则重定向到404页面 需要放到路由的最后面
+    { path: "/:pathMatch(.*)", redirect: "/404", name: "notMatch", hidden: true },
   ]
 })
 
